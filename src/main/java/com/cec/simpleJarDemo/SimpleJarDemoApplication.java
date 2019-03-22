a2d3400beea2f71524a1aa45d6f0cbf10b75e427
ENCEC <2436376429@qq.com>
2019-03-22 09:20:57 +0800

    first commit

---
+++ src/main/java/com/cec/simpleJarDemo/SimpleJarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cec.simpleJarDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJarDemoApplication.class, args);
+	}
+
+}
